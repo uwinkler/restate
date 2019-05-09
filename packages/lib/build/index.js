13d7d17916d52a855f19019ad5d6d5fe37be1f31
uwinkler <uli.winkler@gmail.com>
2019-05-09 18:04:27 +0200

    Added missing exports

--- packages/lib/build/index.js
+++ packages/lib/build/index.js
@@ -3,19 +3,21 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./create-store"));
 __export(require("./connect-actions"));
-__export(require("./create-use-message-bus-hook"));
-__export(require("./create-use-observable-hook"));
-__export(require("./create-use-store-state-hook"));
-__export(require("./create-use-store-update-hook"));
-__export(require("./create-use-store-hook"));
-__export(require("./create-use-view-hook"));
-__export(require("./create-with-store-hoc"));
-__export(require("./create-provider"));
-__export(require("./rx-store"));
-__export(require("./subscribe"));
+__export(require("./connect-actions"));
 __export(require("./connect-logger"));
 __export(require("./connect-message-bus"));
 __export(require("./connect-router"));
+__export(require("./create-provider"));
+__export(require("./create-store"));
+__export(require("./create-use-actions-hook"));
+__export(require("./create-use-message-bus-hook"));
+__export(require("./create-use-observable-hook"));
+__export(require("./create-use-store-hook"));
+__export(require("./create-use-store-state-hook"));
+__export(require("./create-use-store-update-hook"));
+__export(require("./create-use-view-hook"));
+__export(require("./create-with-store-hoc"));
+__export(require("./rx-store"));
+__export(require("./subscribe"));
 //# sourceMappingURL=index.js.map
