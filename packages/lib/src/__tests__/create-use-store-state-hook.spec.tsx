33bb93819c938f0a904a53cdf8099323bc5522fe
uwinkler <uli.winkler@gmail.com>
2019-05-10 14:20:11 +0200

    Try to make the store hook more convinient

---
+++ packages/lib/src/__tests__/create-use-store-state-hook.spec.tsx
@@ -0,0 +1,107 @@
+import { createStore } from "../create-store"
+import { createProvider } from "../create-provider"
+import React from "react"
+import renderer from "react-test-renderer"
+import { createUseStoreStateHook } from "../create-use-store-state-hook"
+
+it("should create default hook", () => {
+  const state = { value: 1 }
+  const store = createStore({ state })
+  const AppStoreProvider = createProvider(store)
+  const useAppStore = createUseStoreStateHook(AppStoreProvider)
+
+  const TestComponent: React.FC = () => {
+    const x = useAppStore(s => s)
+    return <div>{x.value}</div>
+  }
+
+  const component = renderer.create(
+    <AppStoreProvider.Provider value={store}>
+      <TestComponent />
+    </AppStoreProvider.Provider>
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchInlineSnapshot(`
+            <div>
+              1
+            </div>
+      `)
+})
+
+it("sub-state 1", () => {
+  const state = { value: 1 }
+  const store = createStore({ state })
+  const AppStoreProvider = createProvider(store)
+  const useAppStore = createUseStoreStateHook(AppStoreProvider)
+
+  const TestComponent: React.FC = () => {
+    const value = useAppStore(s => s.value)
+    return <div>{value}</div>
+  }
+
+  const component = renderer.create(
+    <AppStoreProvider.Provider value={store}>
+      <TestComponent />
+    </AppStoreProvider.Provider>
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchInlineSnapshot(`
+        <div>
+          1
+        </div>
+    `)
+})
+
+it("sub-state 2", () => {
+  const state = { subState: { value: 1 } }
+  const store = createStore({ state })
+  const AppStoreProvider = createProvider(store)
+  const useAppStoreSubState = createUseStoreStateHook(
+    AppStoreProvider,
+    s => s.subState
+  )
+
+  const TestComponent: React.FC = () => {
+    const subState = useAppStoreSubState(s => s)
+    return <div>{subState.value}</div>
+  }
+
+  const component = renderer.create(
+    <AppStoreProvider.Provider value={store}>
+      <TestComponent />
+    </AppStoreProvider.Provider>
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchInlineSnapshot(`
+        <div>
+          1
+        </div>
+    `)
+})
+
+it("sub-state 1+2", () => {
+  const state = { subState: { value: 1 } }
+  const store = createStore({ state })
+  const AppStoreProvider = createProvider(store)
+  const useAppStoreSubState = createUseStoreStateHook(
+    AppStoreProvider,
+    s => s.subState
+  )
+
+  const TestComponent: React.FC = () => {
+    const value = useAppStoreSubState(s => s.value)
+    return <div>{value}</div>
+  }
+
+  const component = renderer.create(
+    <AppStoreProvider.Provider value={store}>
+      <TestComponent />
+    </AppStoreProvider.Provider>
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchInlineSnapshot(`
+          <div>
+            1
+          </div>
+      `)
+})
--- packages/lib/src/create-use-store-state-hook.ts
+++ packages/lib/src/create-use-store-state-hook.ts
@@ -3,22 +3,48 @@ import { distinctUntilChanged } from "rxjs/operators"
 import { RxStore } from "./rx-store"
 import { useContext, useEffect, useMemo, useState } from "react"
 
+//
+// Types
+
 type RxStoreContext<S> = React.Context<RxStore<S>>
+
 type SelectorFunction<S, T> = (state: S) => T
 
-type IdentifySelectorFunction<S> = (state: S) => S
-const identifySelectorFunction: IdentifySelectorFunction<any> = state => state
+const identifySelectorFunction: SelectorFunction<any, any> = state => state
 
-export function createUseStoreStateHook<S>(context: RxStoreContext<S>) {
-  function useStore<T extends object>(
-    selectorFunction?: SelectorFunction<S, T>
-  ): T {
+export type UseStoreHookWithSelectorFunction<S> = <T>(
+  selectorFunction?: SelectorFunction<S, T>
+) => T
+
+export type UseStoreHookWithoutSelectorFunction<S> = () => S
+
+export type UseStoreStateHook<S> =
+  | UseStoreHookWithSelectorFunction<S>
+  | UseStoreHookWithoutSelectorFunction<S>
+
+export function createUseStoreStateHook<S>(
+  context: React.Context<RxStore<S>>
+): UseStoreStateHook<S>
+
+export function createUseStoreStateHook<S, SUB_STATE>(
+  context: React.Context<RxStore<S>>,
+  outerSelector: SelectorFunction<S, SUB_STATE>
+): UseStoreStateHook<SUB_STATE>
+
+//
+// Implementation
+
+export function createUseStoreStateHook<S, SUB_STATE>(
+  context: RxStoreContext<S>,
+  outerSelector: SelectorFunction<S, SUB_STATE> = identifySelectorFunction
+) {
+  function useStore<T>(selectorFunction?: SelectorFunction<SUB_STATE, T>): T {
     const _store = useContext(context)
     const selector = selectorFunction
       ? selectorFunction
       : identifySelectorFunction
     const state$ = _store.state$
-    const startValue = selector(state$.value)
+    const startValue = selector(outerSelector(state$.value))
 
     const [value, setValue] = useState<T>(startValue)
 
@@ -28,7 +54,7 @@ export function createUseStoreStateHook<S>(context: RxStoreContext<S>) {
 
     useEffect(() => {
       const subscription = state$.subscribe(nextStateValue => {
-        const nextSubValue = selector(nextStateValue)
+        const nextSubValue = selector(outerSelector(nextStateValue))
         output$.next(nextSubValue)
       })
 
@@ -46,3 +72,7 @@ export function createUseStoreStateHook<S>(context: RxStoreContext<S>) {
 
   return useStore
 }
+
+// export function createUseStoreStateHook<S>(
+//   context: RxStoreContext<S>
+// ): CreateUseStoreStateHook
