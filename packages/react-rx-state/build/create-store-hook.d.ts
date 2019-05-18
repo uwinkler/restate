1a4f531f13b67edeb3e60203a91bfd4880237835
uli.winkler <uli.winkler@gmail.com>
2019-05-18 09:06:24 +0200

    PUBLISH

--- packages/react-rx-state/build/create-store-hook.d.ts
+++ packages/react-rx-state/build/create-store-hook.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="react" />
 import { RxStore } from "./rx-store";
-export declare function createUseStoreHook<S>(provider: React.Context<RxStore<S>>): () => RxStore<S>;
+export declare function createStoreHook<S>(provider: React.Context<RxStore<S>>): () => RxStore<S>;
 //# sourceMappingURL=create-store-hook.d.ts.map
--- packages/react-rx-state/build/create-store-hook.js
+++ packages/react-rx-state/build/create-store-hook.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
-function createUseStoreHook(provider) {
+function createStoreHook(provider) {
     return function useStore() {
         var store = react_1.useContext(provider);
         return store;
     };
 }
-exports.createUseStoreHook = createUseStoreHook;
+exports.createStoreHook = createStoreHook;
 //# sourceMappingURL=create-store-hook.js.map
