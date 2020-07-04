7c55a50ae3815e6f204a4c7cdf787a36d9019e71
uli.winkler <uli.winkler@gmail.com>
2020-07-04 17:48:50 +0200

    Fix immer

--- packages/core/src/rx-store.ts
+++ packages/core/src/rx-store.ts
@@ -1,7 +1,9 @@
-import { createDraft, finishDraft, Patch } from "immer"
+import { createDraft, finishDraft, Patch, enableAllPlugins } from "immer"
 import { BehaviorSubject, queueScheduler } from "rxjs"
 import { observeOn } from "rxjs/operators"
 import { Message, RESTATE_UPDATE_MESSAGE } from "./message"
+
+enableAllPlugins()
 
 export type UpdateFunction<S> = (subState: S) => void
 
@@ -11,9 +13,7 @@ interface MiddlewareProps<S, MESSAGE extends Message> {
   message: MESSAGE
 }
 
-export type Middleware<S, MESSAGE extends Message> = (
-  props: MiddlewareProps<S, MESSAGE>
-) => any
+export type Middleware<S, MESSAGE extends Message> = (props: MiddlewareProps<S, MESSAGE>) => any
 
 export interface RxStoreOptions {
   freeze: boolean
@@ -52,10 +52,7 @@ export class RxStore<STATE, MESSAGES extends Message> {
     return new RxStore(state, middleware, options)
   }
 
-  next(
-    updateFunctionOrNextState: UpdateFunction<STATE> | STATE,
-    message: MESSAGES = RESTATE_UPDATE_MESSAGE as any
-  ) {
+  next(updateFunctionOrNextState: UpdateFunction<STATE> | STATE, message: MESSAGES = RESTATE_UPDATE_MESSAGE as any) {
     try {
       const currentStatePackage = this._state$.value
       const isUpdateFunction = updateFunctionOrNextState instanceof Function
@@ -78,7 +75,7 @@ export class RxStore<STATE, MESSAGES extends Message> {
         middleware: this._middleware,
         nextState: draft,
         currentState: this.state,
-        message
+        message,
       })
 
       let _patches: Patch[] | null = null
@@ -93,7 +90,7 @@ export class RxStore<STATE, MESSAGES extends Message> {
         message,
         state: nextState,
         patches: _patches,
-        inversePatches: _inversePatches
+        inversePatches: _inversePatches,
       }
 
       this._state$.next(nextStatePackage)
@@ -136,7 +133,7 @@ function recursiveMiddlewareHandler<STATE, MESSAGES extends Message>({
   middleware,
   nextState,
   currentState,
-  message
+  message,
 }: RecursiveMiddlewareHandlerProps<STATE, MESSAGES>): any {
   if (middleware.length === 0) {
     return
@@ -147,7 +144,7 @@ function recursiveMiddlewareHandler<STATE, MESSAGES extends Message>({
   nextMiddleware({
     nextState,
     currentState,
-    message
+    message,
   })
 
   const remainingMiddleware = middleware.slice(1, middleware.length)
@@ -156,6 +153,6 @@ function recursiveMiddlewareHandler<STATE, MESSAGES extends Message>({
     middleware: remainingMiddleware,
     nextState,
     currentState,
-    message
+    message,
   })
 }
